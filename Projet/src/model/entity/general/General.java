d113a71df631eba45d7ccf2832c07a2bd36dc1a5
Ecness <simonstrzelecki@gmail.com>
2018-09-19 21:57:22 +0200

    Ajout de la classe général
    Oublie de l'ajout de la classe général.

--- Projet/src/model/entity/general/General.java
+++ Projet/src/model/entity/general/General.java
@@ -6,4 +6,54 @@ public class General {
 	private EnumGrade grade;
 	private int puissance;
 	private EnumPassif[] Passif;
+	
+	public General(String nom) {
+		super();
+		this.nom = nom;
+		this.experience = 0;
+		this.grade = null; // a deffinir 
+		this.puissance = 500; // a deffinir 
+		Passif = new EnumPassif[5];// max a deffinir 
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public EnumGrade getGrade() {
+		return grade;
+	}
+
+	public void setGrade(EnumGrade grade) {
+		this.grade = grade;
+	}
+
+	public int getPuissance() {
+		return puissance;
+	}
+
+	public void setPuissance(int puissance) {
+		this.puissance = puissance;
+	}
+
+	public EnumPassif[] getPassif() {
+		return Passif;
+	}
+
+	public void setPassif(EnumPassif[] passif) {
+		Passif = passif;
+	}
+	
 }
