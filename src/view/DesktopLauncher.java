ed8bc0e6d9c2a4062b07e2645a1b69240dc6973d
Julien_B <julien.basquin@hotmail.com>
2019-03-12 11:50:19 +0100

    Adaptation de la résolution à l'écran + fullscreen par défaut

--- src/view/DesktopLauncher.java
+++ src/view/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -8,9 +11,11 @@ import view.launcher.Project;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		config.foregroundFPS = 60;
-		config.width = 1600;
-		config.height = 900;
+		config.width = (int) screenSize.getWidth();
+		config.height = (int) screenSize.getHeight();
+		config.fullscreen = true;
 		new LwjglApplication(new Project(), config);
 	}
 }
