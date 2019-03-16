0dbca01d4451fdf39d7f0dee6df13a63a29ddf10
Julien_B <julien.basquin@hotmail.com>
2019-03-16 10:02:35 +0100

    Prise en compte des technologies pour les bâtiments planète + retrait des bâtiments déjà construits de la liste des constructions

--- src/view/galaxie/systeme/planete/SelectBatimentPlanete.java
+++ src/view/galaxie/systeme/planete/SelectBatimentPlanete.java
@@ -1,7 +1,9 @@
 package view.galaxie.systeme.planete;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -16,6 +18,7 @@ import com.badlogic.gdx.utils.IntMap.Entry;
 import controller.boutons.ErrorButtonSelectBatiment;
 import model.EnumRessource;
 import model.batiment.BatimentPlanete;
+import model.batiment.BatimentVille;
 import model.batiment.ListBatiment;
 import model.carte.stellaire.Planete;
 import view.launcher.Project;
@@ -47,41 +50,51 @@ public class SelectBatimentPlanete extends ScrollPane{
 
 				ListBatiment listeBatiments = parser.fromJson(ListBatiment.class, file);
 
-				for (Entry<BatimentPlanete> batiment : listeBatiments.getBatimentsPlanete().entries()) {
-					//Correction type lecture json (de String vers EnumRessource)
-					Map<EnumRessource, Integer> bonus = new HashMap<EnumRessource, Integer>();
-					Map<EnumRessource, Integer> cout = new HashMap<EnumRessource, Integer>();
-
-					for (EnumRessource ressource : EnumRessource.values()) {
-						bonus.put(ressource, batiment.value.getBonus().get(ressource.name()));
-						cout.put(ressource, batiment.value.getCout().get(ressource.name()));
+				List<String> listeBatimentsPlanete = new ArrayList<String>();
+				for (BatimentPlanete batiment : planete.getTBatiment()) {
+					if (batiment != null) {
+						listeBatimentsPlanete.add(batiment.getNom());
 					}
+				}
 
-					batiment.value.setBonus(bonus);
-					batiment.value.setCout(cout);
+				for (Entry<BatimentPlanete> batiment : listeBatiments.getBatimentsPlanete().entries()) {
+					if (!listeBatimentsPlanete.contains(batiment.value.getNom()) && 
+							(planete.getJoueur().getTechnology().getScienceBatiment().get(listeBatiments.getBatimentsVille().get(batiment.key).getTechNecessaire()).isRechercher())) {
+						//Correction type lecture json (de String vers EnumRessource)
+						Map<EnumRessource, Integer> bonus = new HashMap<EnumRessource, Integer>();
+						Map<EnumRessource, Integer> cout = new HashMap<EnumRessource, Integer>();
 
-					TextButton button = new TextButton(batiment.value.getNom(), Project.skin);
-					//Le bouton est nommé selon sa clé dans la liste des batiments
-					button.setName("" + batiment.key);
-
-					button.addListener(new ClickListener() {
-
-						@Override
-						public void clicked(InputEvent event, float x, float y) {
-							//Récupération du bâtiment selon son nom (clé de la map)
-							if (planete.verifConstructionBatiment(listeBatiments.getBatimentsPlanete().get(Integer.parseInt(button.getName())), emplacement)) {
-								planete.constructionBatiment(listeBatiments.getBatimentsPlanete().get(Integer.parseInt(button.getName())), emplacement);
-								clear();
-								planete.setReDraw(true);
-							} else {
-								//Gestion du bouton d'erreur en cas de ressources insuffisantes
-								setActor(new ErrorButtonSelectBatiment(planete, skin));
-							}
+						for (EnumRessource ressource : EnumRessource.values()) {
+							bonus.put(ressource, batiment.value.getBonus().get(ressource.name()));
+							cout.put(ressource, batiment.value.getCout().get(ressource.name()));
 						}
 
-					});
+						batiment.value.setBonus(bonus);
+						batiment.value.setCout(cout);
 
-					batiments.addActor(button);
+						TextButton button = new TextButton(batiment.value.getNom(), Project.skin);
+						//Le bouton est nommé selon sa clé dans la liste des batiments
+						button.setName("" + batiment.key);
+
+						button.addListener(new ClickListener() {
+
+							@Override
+							public void clicked(InputEvent event, float x, float y) {
+								//Récupération du bâtiment selon son nom (clé de la map)
+								if (planete.verifConstructionBatiment(listeBatiments.getBatimentsPlanete().get(Integer.parseInt(button.getName())), emplacement)) {
+									planete.constructionBatiment(listeBatiments.getBatimentsPlanete().get(Integer.parseInt(button.getName())), emplacement);
+									clear();
+									planete.setReDraw(true);
+								} else {
+									//Gestion du bouton d'erreur en cas de ressources insuffisantes
+									setActor(new ErrorButtonSelectBatiment(planete, skin));
+								}
+							}
+
+						});
+
+						batiments.addActor(button);
+					}
 				}
 			} catch (Exception e) {
 				System.out.println(e.toString() + " dans view/galaxie/SelectBatiment");
