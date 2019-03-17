ba975edbfbd279171aded534891f9a64fcda7ea1
Julien_B <julien.basquin@hotmail.com>
2019-03-17 15:07:44 +0100

    Préparation de la classe de sauvegarde / chargement d'une partie

---
+++ src/model/util/Sauvegarde.java
@@ -0,0 +1,12 @@
+package model.util;
+
+public class Sauvegarde {
+
+	public static void sauvegarder() {
+		//TODO Implémenter le mécanisme de sauvegarde
+	}
+	
+	public static void charger() {
+		//TODO Implémenter le mécanisme de chargement
+	}
+}
